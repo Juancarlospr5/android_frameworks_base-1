734c1acee9bd7998d01fe8dc4edb8f5e6ace16dc
beanstown106 <nbenis106@gmail.com>
2016-01-14 05:56:50 -0500

    Fingerprint authentication vibration [1/2]
    had a few requests for this, as most devices only vibrate when fingerprint authentication is unsucesful so made it configurable
    
    @xyyx: adapted to 8.1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4161,6 +4161,13 @@ public final class Settings {
        public static final String BATTERY_LIGHT_REALLYFULL_COLOR = "battery_light_reallyfull_color";
 
         /**
+         * whether to enable or disable vibration on succesful fingerprint auth
+         *
+         * @hide
+         */
+        public static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
