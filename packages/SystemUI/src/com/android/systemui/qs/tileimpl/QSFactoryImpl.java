1b0d67a06ea1d7f0710f674057234c62ac44389c
Yoshinori Hirano <yoshinori.hirano@sonymobile.com>
2016-09-18 22:33:08 +0200

    Add Sound tile to Quick Settings
    abc ezio84: adapt to O, show volume panel with long press,
    use full dnd (no interruptions) as silent mode
    
    @beanstown106: Sound Tile improvements
    *added longpress action
    *added more descriptive labels for each state instead of always saying sound
    
    ezio84: Adapt for N
    
    Signed-off-by: xyyx <xyyx@mail.ru>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -49,7 +49,7 @@ import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
-
+import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.VolumeTile;
 import com.android.systemui.qs.tiles.WifiTile;
@@ -96,6 +96,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("usb_tether")) return new UsbTetherTile(mHost);
         else if (tileSpec.equals("volume_panel")) return new VolumeTile(mHost);
         else if (tileSpec.equals("screenshot")) return new ScreenshotTile(mHost);
+        else if (tileSpec.equals("sound")) return new SoundTile(mHost);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(mHost, tileSpec);
