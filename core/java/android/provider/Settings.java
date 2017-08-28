89a0e989794b8cbabcf3fd019c4ebedb13d82e2b
xyyx <xyyx@mail.ru>
2017-08-28 17:17:28 +0800

    Mtp_Dirty_Hack: Use mtp by default in oreo too)
    Enabled by default
    
    Change-Id: Ifce53549b36f872bc302a847380f3a07236b7260

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3960,6 +3960,14 @@ public final class Settings {
          public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
+         * Whether to use the MTP by default after connecting to PC
+         * @hide
+         */
+        public static final String MTP_DIRTY_HACK = "mtp_dirty_hack";
+        /** @hide */
+        public static final String MTP_DIRTY_HACK_SAVE = "mtp_dirty_hack_save";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
