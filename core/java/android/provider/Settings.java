07921dd2201d0ccde89854c36edaf719e23e3276
maxwen <max.weninger@gmail.com>
2017-09-23 15:40:18 +0800

    [1/2] base: config to disable power menu on secure lock screen

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7330,6 +7330,13 @@ public final class Settings {
         public static final String DEVICE_THEME = "device_theme";
 
         /**
+         * Disable power menu on secure lock screens
+         *
+         * @hide
+         */
+        public static final String LOCK_POWER_MENU_DISABLED = "lockscreen_power_menu_disabled";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
