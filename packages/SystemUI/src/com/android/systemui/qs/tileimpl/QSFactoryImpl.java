aebe9a834babe8e1817de58f723c639e811a041c
mydongistiny <jaysonedson@gmail.com>
2017-12-23 22:46:44 -0800

    QS: Add CPUInfo toggle tile
    Change-Id: I84fcdff1c29c0bcd3f85d60b4ccba25288ebbddd
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Dabug123 <clarkbrunswick55@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -32,6 +32,7 @@ import com.android.systemui.qs.tiles.CaffeineTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
+import com.android.systemui.qs.tiles.CPUInfoTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.ExpandedDesktopTile;
@@ -107,6 +108,7 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("hwkeys")) return  new HWKeysTile(mHost);
         else if (tileSpec.equals("expanded_desktop")) return new ExpandedDesktopTile(mHost);
         else if (tileSpec.equals("weather")) return new WeatherTile(mHost);
+        else if (tileSpec.equals("cpuinfo")) return new CPUInfoTile(mHost);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(mHost, tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(mHost, tileSpec);
