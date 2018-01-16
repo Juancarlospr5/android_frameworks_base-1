4db9374c1161c5328226aad29a7512651ef0e016
beanstown106 <nbenis106@gmail.com>
2018-01-16 14:00:07 +0100

    Custom Carrier Label & Carrier Label Placement [1/2]

--- core/java/com/android/internal/util/arsenic/ArsenicUtils.java
+++ core/java/com/android/internal/util/arsenic/ArsenicUtils.java
@@ -33,10 +33,12 @@ import android.view.WindowManagerGlobal;
 import android.view.InputDevice;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
+import android.content.res.Resources;
 
 import com.android.internal.statusbar.IStatusBarService;
 
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Some custom utilities
@@ -133,5 +135,10 @@ public class ArsenicUtils {
             }
         }
         return null;
+    }
+
+    public static boolean isChineseLanguage() {
+       return Resources.getSystem().getConfiguration().locale.getLanguage().startsWith(
+               Locale.CHINESE.getLanguage());
     }
 }
