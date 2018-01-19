ed5d0fcd5515c106413030efe4c99d4686417c52
wangjing <vennnice520@gmail.com>
2018-01-19 11:20:50 +0800

    Fix IllegalStateException issues in Setttings
    IllegalStateException caused by the close method that actively invokes the MemoryIntArray object
    
    Bug: b/72180893
    
    Change-Id: If6e2a98edd1a226ef0063aff2ff52e11a6aa0da9
    Signed-off-by: wangjing <vennnice520@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2130,7 +2130,7 @@ public final class Settings {
                                                     + cr.getPackageName() + " and user:"
                                                     + userHandle + " with index:" + index);
                                         }
-                                        if (mGenerationTracker != null) {
+                                        if (mGenerationTracker != null && !Settings.isInSystemServer()) {
                                             mGenerationTracker.destroy();
                                         }
                                         mGenerationTracker = new GenerationTracker(array, index,
@@ -2142,7 +2142,9 @@ public final class Settings {
                                                     GenerationTracker generationTracker =
                                                             mGenerationTracker;
                                                     mGenerationTracker = null;
-                                                    generationTracker.destroy();
+                                                    if(!Settings.isInSystemServer()){
+                                                       generationTracker.destroy();
+                                                    }
                                                     mValues.clear();
                                                 }
                                             }
@@ -2213,7 +2215,7 @@ public final class Settings {
 
         public void clearGenerationTrackerForTest() {
             synchronized (NameValueCache.this) {
-                if (mGenerationTracker != null) {
+                if (mGenerationTracker != null && !Settings.isInSystemServer()) {
                     mGenerationTracker.destroy();
                 }
                 mValues.clear();
