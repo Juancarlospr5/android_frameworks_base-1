15db753eccc05bcef67d940efa6135a65ee7754c
Alex Cruz <du.alexcruz@gmail.com>
2018-01-25 21:18:45 +0530

    Theme DUI dialogs & things [1/2]
    - Bring in DUI overlay to theme some elements that were left out
    from the 'android' overlay. This one themes systemui
    
    Change-Id: Ie24e277be32e40abb8e4b770ad383079101b56a4
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -5017,6 +5017,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                         useDarkTheme, mCurrentUserId);
                 mOverlayManager.setEnabled("com.android.settings.theme.dark",
                         useDarkTheme, mCurrentUserId);
+                mOverlayManager.setEnabled("com.android.dui.theme.dark",
+                        useDarkTheme, mCurrentUserId);
             } catch (RemoteException e) {
                 Log.w(TAG, "Can't change theme", e);
             }
@@ -5032,6 +5034,8 @@ public class StatusBar extends SystemUI implements DemoMode,
                         useBlackTheme, mCurrentUserId);
                 mOverlayManager.setEnabled("com.android.settings.theme.black",
                         useBlackTheme, mCurrentUserId);
+                mOverlayManager.setEnabled("com.android.dui.theme.black",
+                        useDarkTheme, mCurrentUserId);
             } catch (RemoteException e) {
                 Log.w(TAG, "Can't change theme", e);
             }
