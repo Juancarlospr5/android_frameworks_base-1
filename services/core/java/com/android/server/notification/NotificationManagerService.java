657ba9cf8f1022144c4093fed3ef1d1328479ed8
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2018-02-11 09:48:15 +0100

    base: fix notification sound for some apps if Smart notification sound enabled
    e.g. latest whatsapp
    
    notifications from same pkg can have the same currentTime
    the notification key is constructed like
    
    String sbnKey = user.getIdentifier() + "|" + pkg + "|" + id + "|" + tag + "|" + uid;
    
    So those notification will be separated using key
    
    Change-Id: I3afd4147de3d4e2fb54103d6fbfba06fea840328

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -337,7 +337,9 @@ public class NotificationManagerService extends SystemService {
     // The last key in this list owns the hardware.
     ArrayList<String> mLights = new ArrayList<>();
 
+    @GuardedBy("mNotificationLock")
     private HashMap<String, Long> mAnnoyingNotifications = new HashMap<String, Long>();
+
     private long mAnnoyingNotificationThreshold = -1;
 
     private AppOpsManager mAppOps;
@@ -3986,22 +3988,23 @@ public class NotificationManagerService extends SystemService {
         }
     }
 
-    private boolean notificationIsAnnoying(String pkg) {
-        if (pkg == null
+    @GuardedBy("mNotificationLock")
+    private boolean notificationIsAnnoying(String key, String pkg) {
+        if (key == null
                 || mAnnoyingNotificationThreshold <= 0
-                || "android".equals(pkg)) {
+                || (pkg != null && "android".equals(pkg))) {
             return false;
         }
 
         long currentTime = System.currentTimeMillis();
-        if (mAnnoyingNotifications.containsKey(pkg)
-                && (currentTime - mAnnoyingNotifications.get(pkg)
+        if (mAnnoyingNotifications.containsKey(key)
+                && (currentTime - mAnnoyingNotifications.get(key)
                 < mAnnoyingNotificationThreshold)) {
             // less than threshold; it's an annoying notification!!
             return true;
         } else {
             // not in map or time to re-add
-            mAnnoyingNotifications.put(pkg, currentTime);
+            mAnnoyingNotifications.put(key, currentTime);
             return false;
         }
     }
@@ -4103,7 +4106,7 @@ public class NotificationManagerService extends SystemService {
 
         if (aboveThreshold && isNotificationForCurrentUser(record)) {
 
-            if (mSystemReady && mAudioManager != null && !notificationIsAnnoying(pkg)) {
+            if (mSystemReady && mAudioManager != null && !notificationIsAnnoying(key, pkg)) {
                 Uri soundUri = record.getSound();
                 hasValidSound = soundUri != null && !Uri.EMPTY.equals(soundUri);
                 long[] vibration = record.getVibration();
