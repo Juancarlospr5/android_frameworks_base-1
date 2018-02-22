9d2a59151cdba3ed468f0f6a43fb35bef2ab8d6c
arsenicrom <tumayordeseo5@hotmail.com>
2018-02-22 03:17:32 -0400

    Fix compilation

--- services/core/java/com/android/server/statusbar/StatusBarManagerService.java
+++ services/core/java/com/android/server/statusbar/StatusBarManagerService.java
@@ -821,7 +821,7 @@ public class StatusBarManagerService extends IStatusBarService.Stub {
      * Allows the status bar to reboot the device.
      */
     @Override
-    public void reboot(boolean safeMode) {
+    public void reboot(boolean safeMode, String reason) {
         enforceStatusBarService();
         long identity = Binder.clearCallingIdentity();
         try {
