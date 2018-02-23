1c5887f9380978c50a4a4ffe8ff9eb67c8e4f139
SpiritCroc <spiritcroc@gmail.com>
2018-02-23 15:33:58 +0100

    Music ticker: once per track is enough
    Remember the track that last was ticker-ed, and only ticker again
    if actually something changed, instead of spamming with tickers
    so that the status bar gets unusable.
    
    --------
    
    abc/du edit: keep previous track infos when pausing the music
    so playing it again doesn't tick the same infos.
    reset saved infos when dropping the media session (closing the player)
    so a new player session can tick track infos even if it's the same
    track
    
    Change-Id: I5af6a08ba7c3375fc55f49720dcc2e69c88e2603
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -733,6 +733,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         @Override
         public void onSessionDestroyed() {
             super.onSessionDestroyed();
+            if (mTicker != null) {
+                mTicker.resetShownMediaMetadata();
+            }
             setMediaPlaying();
         }
     };
@@ -2632,6 +2635,9 @@ public class StatusBar extends SystemUI implements DemoMode,
                 mMediaController = controller;
                 mMediaController.registerCallback(mMediaListener);
                 mMediaMetadata = mMediaController.getMetadata();
+                if (mTicker != null) {
+                    mTicker.resetShownMediaMetadata();
+                }
                 setMediaPlaying();
 
                 if (DEBUG_MEDIA) {
