01b1b7c2727b606f37aafcffe03659642e097cd5
arsenicrom <tumayordeseo5@hotmail.com>
2018-03-23 02:49:31 -0400

    Allow USB mode dialog to be enabled/disabled [2/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4112,6 +4112,11 @@ public final class Settings {
         public static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
 
         /**
+         * @hide
+         */
+        public static final String SHOW_USB_MODE_DIALOG = "show_usb_mode_dialog";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
