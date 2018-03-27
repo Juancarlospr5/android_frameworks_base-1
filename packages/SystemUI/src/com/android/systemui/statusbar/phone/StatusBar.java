b2a74d93cd36f6891d1cea3e34707fefffc6618a
mydongistiny <jaysonedson@gmail.com>
2018-03-26 19:08:05 -0700

    base: Hide upper shortcuts when phone is dozing
    Change-Id: Icb827175957fff7d0953d72243e6e64e9db6e5d5
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -168,6 +168,7 @@ import com.android.internal.widget.LockPatternUtils;
 import com.android.internal.widget.MessagingGroup;
 import com.android.internal.widget.MessagingMessage;
 import com.android.keyguard.KeyguardHostView.OnDismissAction;
+import com.android.keyguard.KeyguardShortcuts;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
 import com.android.keyguard.ViewMediatorCallback;
@@ -509,6 +510,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     private int mNotificationStyle;
     private boolean mAmbientMediaPlaying;
+    KeyguardShortcuts mKeyguardShortcuts;
 
     /**
      * Helper that is responsible for showing the right toast when a disallowed activity operation
@@ -977,6 +979,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         mAboveShelfObserver.setListener(mStatusBarWindow.findViewById(
                 R.id.notification_container_parent));
         mKeyguardStatusBar = mStatusBarWindow.findViewById(R.id.keyguard_header);
+        mKeyguardShortcuts = (KeyguardShortcuts) mStatusBarWindow.findViewById(R.id.shortcuts);
 
         mNotificationIconAreaController = SystemUIFactory.getInstance()
                 .createNotificationIconAreaController(context, this);
@@ -4744,6 +4747,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         mNotificationPanel.setDozing(mDozing, animate);
         mVisualizerView.setDozing(mDozing);
         updateQsExpansionEnabled();
+        mKeyguardShortcuts.setDozing(mDozing);
         Trace.endSection();
     }
 
