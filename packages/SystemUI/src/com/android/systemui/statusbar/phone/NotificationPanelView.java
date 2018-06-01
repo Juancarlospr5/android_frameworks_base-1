73b05e1f2696065f4c05cd71cbb18adfa48cd3b2
maxwen <max.weninger@gmail.com>
2018-06-01 17:41:07 +0200

    SystemUI: refactor quick settings expand fix
    not only for dt2s - make sure qs cant stay expanded once
    keyguard is started
    
    Change-Id: Ie8778544a529a93ccb0afb25f3a30a3ba50017dc
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1259,6 +1259,7 @@ public class NotificationPanelView extends PanelView implements
             }
         }
         if (keyguardShowing) {
+            mQsExpandImmediate = false;
             updateDozingVisibilities(false /* animate */);
         }
         resetVerticalPanelPosition();
