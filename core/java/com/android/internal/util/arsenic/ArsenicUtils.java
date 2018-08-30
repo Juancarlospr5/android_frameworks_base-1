6d6b18290a337132d12fa36fd4cfc51e9a5d9268
romanbb <romanbirg@gmail.com>
2018-08-30 11:04:47 +0200

    ArsenicUtils: add isPackageInstalled boolean
    Change-Id: Ib23bdb4cf2f8f0ed909f52ac2dab6e18fe9a91d4

--- core/java/com/android/internal/util/arsenic/ArsenicUtils.java
+++ core/java/com/android/internal/util/arsenic/ArsenicUtils.java
@@ -38,6 +38,9 @@ import android.view.WindowManagerGlobal;
 import java.util.List;
 import java.util.Locale;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+
 import com.android.internal.statusbar.IStatusBarService;
 
 /**
@@ -53,6 +56,25 @@ public class ArsenicUtils {
         if (pm!= null) {
             pm.goToSleep(SystemClock.uptimeMillis());
         }
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
+        if (pkg != null) {
+            try {
+                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+                if (!pi.applicationInfo.enabled && !ignoreState) {
+                    return false;
+                }
+            } catch (NameNotFoundException e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        return isPackageInstalled(context, pkg, true);
     }
 
     public static boolean deviceHasFlashlight(Context ctx) {
