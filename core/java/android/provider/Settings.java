b14452d59ebc228d66dcd5e6ec21b3d429c025d1
mydongistiny <jaysonedson@gmail.com>
2018-10-25 22:55:16 -0700

    SystemUI: Add switch for statusbar wifi data indicators [1/2]
    Since it's enabled by default and some pople complain about it add a way to disable it.
    
    Change-Id: I9ce7ead4caccfe96a982cb45a4c2ed1d54d5039c
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5659,6 +5659,15 @@ public final class Settings {
         public static final String GESTURE_ANYWHERE_SHOW_TRIGGER = "gesture_anywhere_show_trigger";
 
         /**
+         * Enable or disable wifi data activity indicators
+         * @hide
+         */
+        public static final String STATUSBAR_SHOW_WIFI_ACTIVITY = "statusbar_show_wifi_activity";
+
+        /** @hide */
+        private static final Validator STATUSBAR_SHOW_WIFI_ACTIVITY_VALIDATOR = BOOLEAN_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -5735,6 +5744,7 @@ public final class Settings {
             RECENTS_COMPONENT,
             STATUSBAR_HIDE_NOTCH,
             ONE_HAND_MODE_ENABLED,
+            STATUSBAR_SHOW_WIFI_ACTIVITY,
         };
 
         /**
@@ -5881,6 +5891,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(QS_TILE_STYLE);       
             PRIVATE_SETTINGS.add(ONE_HAND_MODE_ENABLED);
             PRIVATE_SETTINGS.add(TOAST_ICON);
+            PRIVATE_SETTINGS.add(STATUSBAR_SHOW_WIFI_ACTIVITY);
         }
 
 
@@ -5996,6 +6007,7 @@ public final class Settings {
             VALIDATORS.put(RECENTS_COMPONENT,RECENTS_COMPONENT_VALIDATOR);
             VALIDATORS.put(STATUSBAR_HIDE_NOTCH, STATUSBAR_HIDE_NOTCH_VALIDATOR);
             VALIDATORS.put(ONE_HAND_MODE_ENABLED, ONE_HAND_MODE_ENABLED_VALIDATOR);
+            VALIDATORS.put(STATUSBAR_SHOW_WIFI_ACTIVITY, STATUSBAR_SHOW_WIFI_ACTIVITY_VALIDATOR);
         }
 
         /**
