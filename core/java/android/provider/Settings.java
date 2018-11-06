36cb7e0895151afc93faec539c7dbaea411929e1
Ritayan Chakraborty <ritayanout@gmail.com>
2018-11-05 18:56:26 -0700

    Pulse: Add bar animation smoothing [1/3]
    Change-Id: I7a3e09cb04689811f944e9fbe32fcbb91ca56edb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5238,6 +5238,13 @@ public final class Settings {
         public static final String FLING_PULSE_LAVALAMP_COLOR_TO = "fling_lavalamp_color_to";
 
         /**
+         * Fling pulse bars smoothing
+         *
+         * @hide
+         */
+        public static final String FLING_PULSE_SMOOTHING_ENABLED = "fling_pulse_smoothing_enabled";
+
+        /**
          * Whether to show the visualizer on the lock screen
          * @hide
          */
