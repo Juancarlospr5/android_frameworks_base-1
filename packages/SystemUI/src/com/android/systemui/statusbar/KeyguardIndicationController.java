AICP/frameworks_base

fwb: Ambient Display battery toggle [1/2]
Change-Id: I1bf444ce211e1c5983f4c151ea38534b41230884

packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -301,10 +301,16 @@ public class KeyguardIndicationController {
                 } else {
                     // Use the high voltage symbol ⚡ (u26A1 unicode) but prevent the system
                     // to load its emoji colored variant with the uFE0E flag
-                    String bolt = "\u26A1\uFE0E";
-                    CharSequence chargeIndicator = (mPowerPluggedIn ? (bolt + " ") : "") +
-                            NumberFormat.getPercentInstance().format(mLevel / 100f);
-                    mTextView.switchIndication(chargeIndicator);
+                    boolean showAmbientBattery = Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.AMBIENT_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT) != 0;
+                    if (showAmbientBattery) {
+                        String bolt = "\u26A1\uFE0E";
+                        CharSequence chargeIndicator = (mPowerPluggedIn ? (bolt + " ") : "") +
+                                NumberFormat.getPercentInstance().format(mLevel / 100f);
+                        mTextView.switchIndication(chargeIndicator);
+                    } else {
+                        mTextView.switchIndication(null);
+                    }
                 }
                 return;
             }

Juancarlospr5/android_frameworks_base-1

packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -311,9 +311,18 @@ public class KeyguardIndicationController {
                         mTextView.switchIndication(indication);
                     }
                 } else {
-                    String percentage = NumberFormat.getPercentInstance()
-                            .format(mBatteryLevel / 100f);
-                    mTextView.switchIndication(percentage);
+                    // Use the high voltage symbol ⚡ (u26A1 unicode) but prevent the system
+                    // to load its emoji colored variant with the uFE0E flag
+                    boolean showAmbientBattery = Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.AMBIENT_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT) != 0;
+                    if (showAmbientBattery) {
+                        String bolt = "\u26A1\uFE0E";
+                        CharSequence chargeIndicator = (mPowerPluggedIn ? (bolt + " ") : "") +
+                                NumberFormat.getPercentInstance().format(mBatteryLevel / 100f);
+                        mTextView.switchIndication(chargeIndicator);
+                    } else {
+                        mTextView.switchIndication(null);
+                    }
                 }
                 return;
             }